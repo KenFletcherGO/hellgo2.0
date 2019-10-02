17c22480f5757e9f91ff9d0bc344d6b75dfe16b4
AK97Z3R0 <artkhv15@protonmail.com>
2019-10-02 10:29:22 -0500

    finishes hello world with help of M.R.

--- makesure/makesure.go
+++ makesure/makesure.go
@@ -2,14 +2,20 @@ package makesure
 
 func Check(s string, f map[string]string) string {
 	var holder string
-	for k, v := range f {
-		if k == s {
-			holder = v
-		} else if len(s) > 2 || len(s) < 2 {
-			holder = "yo"
-		} else {
-			holder = "Yo"
-		}
+	holder = f[s]
+	//if its not the key then the value would be
+	//empty remeber this
+	if holder == "" {
+		holder = "Yo"
 	}
+	/*
+		for k, v := range f {
+			if len(s) == 2 && k == s {
+				holder = v
+			} else {
+				holder = "Yo"
+			}
+		}
+	*/
 	return holder
 }
